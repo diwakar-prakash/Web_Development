5e3260587321cb5c50292400702c36e7144ed10c
diwakar-prakash <sush0697@gmail.com>
2025-11-13 21:13:18 +0530

    Development -- expense-tracker-2 -- backend -- I am working on this expense tracker application. This particularly is the backend part of the application. It is the first commit (1) for the (backend). It is partially complete

---
+++ Projects/expense-tracker-2/backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("DATABASE CONNECTED SUCCESSFULLY");
+    } 
+    catch ( err ) {
+        console.log("DATABASE NOT CONNECTED");
+    }
+}
+
+
+export default connectDB;
+
