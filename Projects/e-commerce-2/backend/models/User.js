d9c5d952fae23f36022112a91102b5da84309fa3
diwakar-prakash <sush0697@gmail.com>
2025-11-16 23:31:23 +0530

    Development -- e-commerce-2 -- backend -- I am working on this e-commerce application. This particularly is the backend part of the application. This is not complete. This is the first (1) commit of this project

---
+++ Projects/e-commerce-2/backend/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String
+    },
+    role : {
+        type : String,
+        enum : ["user", "admin"]
+    }
+})
+
+export default mongoose.model("Users_in_e-commerce-2_application", userSchema);
