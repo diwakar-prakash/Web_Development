7566aaa55bd470f74190e10917f0372c90085912
diwakar-prakash <sush0697@gmail.com>
2025-12-07 23:26:56 +0530

    Development -- chat-app-1 -- backend && frontend -- I am working on this chat application. This is full backend along with frontend. Learnt some group stuff and added that to the code today. This is the second (2) commit of this project

--- Projects/chat-app-1/backend/server.js
+++ Projects/chat-app-1/backend/server.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
-import http from 'http';
+import http from "http";
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -9,36 +9,50 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin : "*"
-    },
-})
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(express.static("../frontend"));
 
-io.on('connection', (socket) => {
-    console.log("Welcome to the Chat Application. Your socket id is :", socket.id);
+io.on("connection", (socket) => {
+  console.log(
+    "Welcome to the Chat Application. Your socket id is :",
+    socket.id
+  );
 
-    socket.on("chat:message", (message) => {
-        console.log(`The message from ${socket.id} is ${message}`);
+  socket.on("join-room", (roomName) => {
 
-        io.emit("chat:message", {
-            id: socket.id,
-            text: message,
-            time: new Date().toISOString()
-        })
+    for(let room of socket.rooms) {
+      if(room !== socket.id) {
+        socket.leave(room);
+      }
+    }
+
+    socket.join(roomName);
+    console.log(`🚪 ${socket.id} joined ${roomName}`);
+
+    socket.to(roomName).emit("user-joined", `${socket.id} joined the room`);
+  });
+
+  socket.on("chat:message", ({room, text}) => {
+    console.log(`The message from ${socket.id} is ${text}`);
+
+    io.to(room).emit("chat:message", {
+      id: socket.id,
+      text,
+      time: new Date().toISOString(),
     });
+  });
 
-    socket.on("disconnect", () => {
-        console.log(`The user with socket id ${socket.id} has been disconnected`);
-    });
+  socket.on("disconnect", () => {
+    console.log(`The user with socket id ${socket.id} has been disconnected`);
+  });
 });
 
-const PORT = process.env.PORT || 5002
+const PORT = process.env.PORT || 5002;
 
 server.listen(PORT, () => {
-    console.log(`The Chat Server is running on the PORT ${PORT}`)
-})
-
-
-
+  console.log(`The Chat Server is running on the PORT ${PORT}`);
+});
--- Projects/chat-app-1/frontend/index.js
+++ Projects/chat-app-1/frontend/index.js
@@ -1,15 +1,32 @@
-const socket = io();
+const socket = io('http://localhost:5002');
 
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messageDiv = document.getElementById('messages');
+const roomButtons = document.querySelectorAll('.room-btn')
 
 let myId = null;
+let currentRoom = 'general';
 
 socket.on('connect', () => {
     myId = socket.id;
+    joinRoom("general");
     console.log("Connected with id : ", myId);
 })
+
+function joinRoom(room) {
+    currentRoom = room;
+    socket.emit('join-room', room);
+
+    messageDiv.innerHTML = "";
+    roomButtons.forEach((btn) => btn.classList.remove("active"));
+    document.querySelector(`[data-room="${room}"]`).classList.add("active");
+}
+
+roomButtons.forEach((btn) => {
+    btn.addEventListener('click', () => joinRoom(btn.dataset.room));
+})
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -17,22 +34,32 @@ form.addEventListener("submit", (e) => {
     if(!text) {
         return;
     }
-    socket.emit("chat:message", text);
+    socket.emit("chat:message", { room : currentRoom, text });
     input.value = "";
 })
 
 socket.on("chat:message", (data) => {
     const div = document.createElement('div');
     div.classList.add("msg");
+
     if(data.id == myId) {
         div.classList.add("me");
         div.textContent = `${data.text}`;
     }
     else {
-        div.classList.add("other");
         div.textContent = `${data.id.slice(0, 5)}: ${data.text}`
     }
 
     messageDiv.appendChild(div);
     messageDiv.scrollTop = messageDiv.scrollHeight;
 })
+
+socket.on('user-joined', (msg) => {
+    const div = document.createElement("div");
+    div.classList.add('msg');
+    div.style.textAlign = "center";
+    div.style.opacity = "0.5";
+    div.textContent = msg;
+    messageDiv.appendChild(div);
+});
+
